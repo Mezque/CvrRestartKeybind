fc3b4d7b0cdea414d3078b4278e47e75eece420b
Mezque <31026406+Mezque@users.noreply.github.com>
2022-08-01 23:38:16 -0300

    added update checker should work I hope? (also ML FIX CONSOLE WRITELINE ON MONO PLEASE :((( (((( :TROLLFACE:)

--- Main.cs
+++ Main.cs
@@ -4,6 +4,9 @@ using System.Diagnostics;
 using System.Threading.Tasks;
 using System.IO;
 using System.Reflection;
+using System.Security.Cryptography;
+using System.Net;
+using System.Text;
 #if MELONLOADER
 using MelonLoader;
 [assembly: MelonInfo(typeof(CvrRestartKeybind.MainML), $"{AsInfo.Title}", $"{AsInfo.Version}", $"{AsInfo.Dev}")]
@@ -21,8 +24,11 @@ namespace CvrRestartKeybind
     internal class MainBiE : BaseUnityPlugin
     {
         internal void Awake()
+
         {
             Logger.LogMessage($"{AsInfo.Title}: V{AsInfo.Version} Started");
+            Logger.LogMessage("Checking For Updates Now.");
+            UpdateNotice.UpdateCheck();
         }
         internal void Update()
         {
@@ -37,7 +43,9 @@ namespace CvrRestartKeybind
     {
         public override void OnApplicationStart()
         {
-            LoggerInstance.Msg($"{AsInfo.Title}: V{AsInfo.Version} Started");
+            LoggerInstance.Msg(ConsoleColor.DarkMagenta,$"{AsInfo.Title}: V{AsInfo.Version} Started");
+            LoggerInstance.Msg(ConsoleColor.DarkMagenta,"Checking For Updates Now.");
+            UpdateNotice.UpdateCheck();
 
         }
         public override void OnUpdate()
@@ -78,6 +86,95 @@ internal class Restarts
         Process.GetCurrentProcess().Kill();
     }
 }
+internal class UpdateNotice
+{
+    internal static void UpdateCheck()
+    {
+        using var sha = SHA256.Create();
+#if MELONLOADER
+        var gitURL = "https://github.com/Mezque/CvrRestartKeybind/releases/latest/download/MlCvrRestartKeybind.dll";
+        var ModDLL = "Mods//MLMicDotRecolour.dll";
+#endif
+
+#if BEPINEX
+        var gitURL = "https://github.com/Mezque/CvrRestartKeybind/releases/latest/download/BiECvrRestartKeybind.dll";
+        var ModDLL = "BepInEx//plugins//MLMicDotRecolour.dll";
+#endif
+        byte[] DllCur = null;
+        byte[] DLLupdate = null;
+        using var wc = new WebClient();
+
+        if (File.Exists(ModDLL))
+        {
+            DllCur = File.ReadAllBytes(ModDLL);
+        }
+        try
+        {
+            DLLupdate = wc.DownloadData($"{gitURL}");
+        }
+        catch (WebException ex)
+        {
+#if MELONLOADER
+            MlLogger.Msg(ConsoleColor.DarkMagenta,$"Unable to check for mod update. \n{ex}");
+#endif
+#if BEPINEX
+            Console.WriteLine($"Unable to check for mod update. \n{ex}");
+#endif
+        }
+        try
+        {
+            string CurModHash = ComputeHash(sha, DllCur);
+            string UpdateModHash = ComputeHash(sha, DLLupdate);
+
+            if (CurModHash != UpdateModHash)
+            {
+#if MELONLOADER
+                MlLogger.Msg(ConsoleColor.DarkMagenta, $"There Is A Mod Update Available At:\n {gitURL}\n Certan Features May NOT Work Until You Update!");
+#endif
+#if BEPINEX
+                Console.Write($"There Is A Mod Update Available At:\n {gitURL}\n Certan Features May NOT Work Until You Update!");
+#endif
+            }
+            else
+            {
+#if MELONLOADER
+                MlLogger.Msg(ConsoleColor.DarkMagenta, "[INFO] No Updates Available. :)");
+#endif
+#if BEPINEX
+                Console.Write("[INFO] No Updates Available. :)");
+#endif
+            }
+        }
+        catch (Exception ex)
+        {
+#if MELONLOADER
+            MlLogger.Msg(ConsoleColor.DarkMagenta, $"Failed To Check For Updates:\n{ex}");
+#endif
+#if BEPINEX
+            Console.Write($"Failed To Check For Updates:\n{ex}");
+#endif
+        }
+    }
+    private static string ComputeHash(HashAlgorithm sha256, byte[] data)
+    {
+        byte[] array = sha256.ComputeHash(data);
+        StringBuilder stringBuilder = new StringBuilder();
+        foreach (byte b in array)
+        {
+            stringBuilder.Append(b.ToString("x2"));
+        }
+        return stringBuilder.ToString();
+    }
+}
+#if MELONLOADER
+internal class MlLogger
+{
+    internal static MelonLogger.Instance Conlog = new($"{AsInfo.Title}", ConsoleColor.White);
+    internal static void Msg(ConsoleColor ConColour, string obj) => Conlog.Msg(ConColour, obj);
+    internal static void Error(string obj) => Conlog.Error(obj);
+    internal static void Warning(string obj) => Conlog.Warning(obj);
+}
+#endif
 internal struct AsInfo
 {
     internal const string Version = "1.0.0.0";
